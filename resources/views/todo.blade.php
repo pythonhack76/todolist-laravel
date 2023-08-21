4d86b6d71e484350d0eee008c4cb9fff0f1de530
pythonhack76 <lrulvoni@gmail.com>
2023-08-21 13:53:29 +0200

    update form

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -8,10 +8,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Hel Patron!</title>
 </head>
 <body>
-<h1>Hello, world!</h1>
+<h1>Hel Patron!</h1>
 <div class="row justify-content-center mt-5">
     <div class="col-lg-6">
         @if(session()->has('success'))
@@ -28,6 +28,19 @@
             @endforeach
         @endif
     </div>
+</div>
+<div class="text-center mt-5">
+    <h2>Aggiungi To Do </h2>
+
+    <form class="row g-3 justify-content-center" method="POST" action="{{route('todos.store')}}">
+        @csrf
+        <div class="col-6">
+            <input type="text" class="form-control" name="title" placeholder="Title">
+        </div>
+        <div class="col-auto">
+            <button type="submit" class="btn btn-primary mb-3">Submit</button>
+        </div>
+    </form>
 </div>
 
 
